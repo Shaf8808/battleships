18ce2dac159f67ad9ec571a0e548f37a88fced3c
Shaf8808 <shadmankhan08@gmail.com>
2023-03-29 12:40:23 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+HIDDEN_BOARD = [[' '] * 10 for x in range(10)]
+GUESS_BOARD = [[' '] * 10 for x in range(10)]
+
+
+letters_to_numbers = {
+    'A': 0,
+    'B': 1,
+    'C': 2,
+    'D': 3,
+    'E': 4,
+    'F': 5,
+    'G': 6,
+    'H': 7,
+    'I': 8,
+    'J': 9
+}
+
+def board():
+    pass
+
+def get_ship_location():
+    
