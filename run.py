02b238e1d96999338ce097c406ebbb3484d185ca
Shaf8808 <shadmankhan08@gmail.com>
2023-04-13 11:53:44 +0100

    Edit input message

--- run.py
+++ run.py
@@ -61,7 +61,7 @@ def get_ship_location():
     """
     while True:
         try:
-            row = input('Please enter a ship row 1-9: ')
+            row = input('Please enter a ship row 1-9: \n')
             if row in "123456789":
                 row = int(row) - 1
                 break
